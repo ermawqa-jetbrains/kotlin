e14f543eb9c6522d05fc2ee3f8e0e1834d7546c9
Oleg Makeev <oleg.makeev@jetbrains.com>
2026-06-23 16:30:30 +0200

    [Analysis API] Deprecate `KaPropertySymbol.isFromPrimaryConstructor`
    The API was deprecated in favor of the new `KaKotlinPropertySymbol.primaryConstructorParameter` endpoint.
    Since `primaryConstructorParameter` is direct and performant, there is no need in keeping the flag.
    Additionally, the symmetric API `KaValueParameterSymbol.generatedPrimaryConstructorProperty` doesn't have any flag alternative.
    
    ^KT-73946

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/symbols/KaVariableSymbol.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/symbols/KaVariableSymbol.kt
@@ -359,6 +359,7 @@ public sealed class KaPropertySymbol : KaVariableSymbol(), KaTypeParameterOwnerS
      *
      * In contrast, `Foo.count` is not declared in the primary constructor.
      */
+    @Deprecated("Use `primaryConstructorParameter` instead.", ReplaceWith("primaryConstructorParameter != null"))
     public abstract val isFromPrimaryConstructor: Boolean
 
     /**
@@ -519,6 +520,11 @@ public abstract class KaSyntheticJavaPropertySymbol : KaPropertySymbol() {
     @KaExperimentalApi
     final override val contextReceivers: List<KaContextReceiver> get() = withValidityAssertion { emptyList() }
     final override val backingFieldSymbol: KaBackingFieldSymbol? get() = withValidityAssertion { null }
+
+    @Deprecated(
+        "Use `primaryConstructorParameter` instead.",
+        replaceWith = ReplaceWith("primaryConstructorParameter != null")
+    )
     final override val isFromPrimaryConstructor: Boolean get() = withValidityAssertion { false }
     override val origin: KaSymbolOrigin get() = withValidityAssertion { KaSymbolOrigin.JAVA_SYNTHETIC_PROPERTY }
 
@@ -682,7 +688,6 @@ public abstract class KaValueParameterSymbol : KaParameterSymbol() {
      * The associated generated [KaPropertySymbol] if this value parameter corresponds to a `val` or `var` property declaration in a primary
      * constructor.
      *
-     * @see KaPropertySymbol.isFromPrimaryConstructor
      * @see KaKotlinPropertySymbol.primaryConstructorParameter
      */
     public open val generatedPrimaryConstructorProperty: KaKotlinPropertySymbol? get() = null
--- analysis/symbol-light-classes/src/org/jetbrains/kotlin/light/classes/symbol/classes/symbolLightClassUtils.kt
+++ analysis/symbol-light-classes/src/org/jetbrains/kotlin/light/classes/symbol/classes/symbolLightClassUtils.kt
@@ -129,7 +129,7 @@ internal fun KaSession.createMethods(
     suppressStatic: Boolean = false,
     staticsFromCompanion: Boolean = false,
 ) {
-    val [ctorProperties, regularMembers] = declarations.partition { it is KaPropertySymbol && it.isFromPrimaryConstructor }
+    val [ctorProperties, regularMembers] = declarations.partition { it is KaPropertySymbol && it.primaryConstructorParameter != null }
 
     fun KaSession.handleDeclaration(declaration: KaCallableSymbol) {
         when (declaration) {
@@ -497,7 +497,7 @@ private fun hasBackingField(property: KaPropertySymbol): Boolean {
     )
 
     if (property.origin.cannotHasBackingField() || property.isStatic) return false
-    if (property.isLateInit || property.isDelegated || property.isFromPrimaryConstructor) return true
+    if (property.isLateInit || property.isDelegated || property.primaryConstructorParameter != null) return true
     val hasBackingFieldByPsi: Boolean? = property.psi?.hasBackingField()
     if (hasBackingFieldByPsi == false) {
         return hasBackingFieldByPsi
@@ -700,7 +700,7 @@ internal fun KaSession.addPropertyBackingFields(
             filter { lightClass.containingClass?.isInterface == true && !it.isJvmField }
         }
 
-    val [ctorProperties, memberProperties] = propertySymbols.partition { it.isFromPrimaryConstructor }
+    val [ctorProperties, memberProperties] = propertySymbols.partition { it.primaryConstructorParameter != null }
     val containerIsObject = containerSymbol is KaClassSymbol && containerSymbol.classKind.isObject
     fun addPropertyBackingField(propertySymbol: KaPropertySymbol) {
         @OptIn(KaExperimentalApi::class)
