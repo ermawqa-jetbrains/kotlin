f6ca97f8826cb849cb90827967fb6f60c3c1a241
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-07-07 08:58:34 +0000

    [FIR] Reduce text range of COMPANION_BLOCK_MEMBER_EXTENSION

--- compiler/testData/diagnostics/tests/companionBlocks/interfaces.kt
+++ compiler/testData/diagnostics/tests/companionBlocks/interfaces.kt
@@ -16,6 +16,8 @@ interface I {
 
         <!PRIVATE_PROPERTY_IN_INTERFACE!>private<!> val private1 = 1
         <!PRIVATE_PROPERTY_IN_INTERFACE!>private<!> var private2 = 2
+        var private3 = 2
+            private set
     }
 }
 
