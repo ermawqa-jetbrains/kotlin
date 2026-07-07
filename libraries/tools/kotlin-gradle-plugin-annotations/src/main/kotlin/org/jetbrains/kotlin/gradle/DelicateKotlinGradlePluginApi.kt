6a12398c247ae8b359109492b295fb71de71c69e
Anton Lakotka <anton.lakotka@jetbrains.com>
2026-07-07 09:05:12 +0200

    [Gradle] Introduce DelicateKotlinGradlePluginApi Opt-In
    Some of KGP Api is not intended for everyday use. And we want to warn
    users about that.
    
    There can be multiple reasons for doing that:
    1. the API is not easily understandable, it has hidden pits, and it
      requires thorough reading of the documentation to understand that.
      a good example is `KotlinSourceSet.dependsOn(KotlinSourceSet)` API.
    
    1. API that allows users to replace default behavior or components
     provided by KGP. For example, different bundlers for JS target or
     custom WASM runtimes.  Replacing defaults can lead to unexpected
     results, since this integration is not covered by tests.
    
    ^KT-87481 Verification Pending

---
+++ libraries/tools/kotlin-gradle-plugin-annotations/src/main/kotlin/org/jetbrains/kotlin/gradle/DelicateKotlinGradlePluginApi.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle
+
+/**
+ * General annotation to mark delicate Kotlin Gradle Plugin API.
+ * API marked with this annotation most likely is not suitable for everyday use.
+ * Yet it is useful and covers some corner cases that may be required by a small number of users.
+ *
+ * If you're unsure about using this API, feel free to ask in Kotlin Slack or Kotlin issue tracker.
+ */
+@RequiresOptIn(
+    level = RequiresOptIn.Level.WARNING,
+    message = "This is delicate Kotlin Gradle Plugin API." +
+            " Make sure you fully understand the use case, applicability area and read documentation about this API."
+)
+annotation class DelicateKotlinGradlePluginApi(val kind: String)
+
+object DelicateKotlinGradlePluginApiKind {
+    /**
+     * API allows replacing the default behavior or component of KGP.
+     * And can break integration with other components. Leading to incorrect or failed builds.
+     */
+    const val REPLACES_DEFAULTS = "DELICATE_KOTLIN_GRADLE_PLUGIN_API_REPLACES_DEFAULTS"
+
+    /**
+     * API is not intuitive and requires a deep understanding of its functionality.
+     * User of the API should read related documentation.
+     */
+    const val REQUIRES_KNOWLEDGE = "DELICATE_KOTLIN_GRADLE_PLUGIN_API_REQUIRES_KNOWLEDGE"
+}
